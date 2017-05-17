79ca4cae80eabe08896d7d4b597f649fe7b4eae0
clement.gomis@epitech.eu <clement.gomis@epitech.eu>
2017-05-17 15:59:06 +0200

    Adding alias loading

--- src/script/alias.c
+++ src/script/alias.c
@@ -5,12 +5,36 @@
 ** Login   <romain.pillot@epitech.net>
 ** 
 ** Started on  Tue May 16 16:12:23 2017 romain pillot
-** Last update Wed May 17 12:28:47 2017 romain pillot
+** Last update Wed May 17 15:58:01 2017 Clement Gomis
 */
 
 #include "minishell.h"
+#include "util.h"
+#include <stdlib.h>
+#include <string.h>
 
-void	load_aliases(t_shell *shell, char **file_content)
+void		load_aliases(t_shell *shell, char **file_content)
 {
-  
+  int		i;
+  char		*str;
+  char		**split;
+  t_alias	*alias;
+  int		length;
+
+  i = -1;
+  while ((str = file_content[++i]))
+    {
+      if (start_withstr(str, "alias"))
+	{
+	  split = splitstr(str, '=');
+	  alias = malloc(sizeof(t_alias));
+	  alias->name = strdup(split[0] + 5);
+	  length = str_length(split[1]);
+	  split[1][length - 1] = 0;
+	  alias->content = strdup(split[1] + 1);
+	  list_add(shell->scripts->aliases, alias);
+	  safe_freesub(split, true);
+	}
+    }  
+		
 }
