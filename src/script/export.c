498accf92d159e29304284a4c1ca8d21e7473a4c
Miolane <quentin.miolane@epitech.eu>
2017-05-17 16:27:20 +0200

    get export from bashrc

--- src/script/export.c
+++ src/script/export.c
@@ -5,12 +5,30 @@
 ** Login   <romain.pillot@epitech.net>
 ** 
 ** Started on  Wed May 17 11:27:29 2017 romain pillot
-** Last update Wed May 17 12:58:44 2017 romain pillot
+** Last update Wed May 17 16:25:32 2017 Miolane
 */
 
 #include "minishell.h"
+#include "util.h"
+#include "environment.h"
 
 void	load_exports(t_shell *shell, char **file_content)
 {
-  
+  char		*str;
+  int		i;
+  char		*buffer[2];
+  char		**hold;
+
+  buffer[1] = 0;
+  i = -1;
+  while (str = file_content[++i])
+    {
+      if (start_withstr("export", str))
+	{
+	  buffer[0] = str + 6;
+	  hold = shell->env;
+	  shell->env = copy_env(hold, buffer);
+	  free_tab(hold);
+	}
+    }
 }
